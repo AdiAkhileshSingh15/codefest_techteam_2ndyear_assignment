74596b04e3957fd7dd4f6a401c1570c22e9d6de6
karankoder <karankr.das.cse22@iitbhu.ac.in>
2023-10-06 22:50:40 +0530

    Assignment Completed

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import Login from './Components/Login.js'
+import Signup from './Components/Signup.js'
+import {  Route, Routes } from "react-router-dom";
+import WorkoutCard from './Components/WorkoutCard.js';
+import { useNavigate } from "react-router-dom";
+import React,{useEffect} from 'react'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import AddWorkout from './Components/AddWorkout.js';
+function App() {
+  const navigate=useNavigate();
+  useEffect(() => {
+    if(!localStorage.getItem("token")){
+      navigate("/Login");
+    }
+    else{
+      navigate("/WorkoutCard");
+    }
+  }, [])
+  
+  return (
+    <>
+    
+      <Routes>
+      <Route exact path='/' element={<Login></Login>}/> 
+        <Route exact path='/Login' element={<Login></Login>}/> 
+        <Route exact path='/Signup' element={<Signup></Signup>}/> 
+        <Route exact path='/WorkoutCard' element={<WorkoutCard></WorkoutCard>}/>
+        <Route exact path='/AddWorkout' element={<AddWorkout></AddWorkout>}/>
+        
+      </Routes>
+      <ToastContainer limit={2}></ToastContainer>
+    </>
+    
+  );
+}
+
+export default App;
