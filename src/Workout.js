dbf9c8958a8ecb2500937e7e352854faa4b4ca46
g.priyanshi <124982515+g-priyanshi@users.noreply.github.com>
2023-10-07 16:03:25 +0530

    corrected error in Workout.js

--- src/Workout.js
+++ src/Workout.js
@@ -24,7 +24,7 @@ const CreateWorkout = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization":"Bearer ${`userToken`}",
+          "Authorization":`Bearer${userToken}`,
           "Accept": "application/json",
         },
         body: JSON.stringify(newWorkout),
